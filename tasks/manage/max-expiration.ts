302b592e48d5deed689e7dbec71db5fdc8d71e8e
he <he@fns>
2023-03-14 18:00:12 +0800

    fix max-expiration

--- tasks/manage/max-expiration.ts
+++ tasks/manage/max-expiration.ts
@@ -1,5 +1,6 @@
 import { task, types } from "hardhat/config";
 import { RegistrarController } from "../../typechain-types";
+import { txParams } from "../common";
 
 task("get-max-expiration", "Get max expiration time of RegistrarController").setAction(async ({ count }, hre) => {
   const regController: RegistrarController = await hre.ethers.getContract("RegistrarController");
@@ -7,18 +8,21 @@ task("get-max-expiration", "Get max expiration time of RegistrarController").set
   if (exp.isZero()) {
     console.log(`maxExpirationTime: unlimited`);
   } else {
-    console.log(`maxExpirationTime: ${exp.toNumber()}`);
+    console.log(`maxExpirationTime: ${new Date(exp.toNumber() * 1000)}`);
   }
 });
 
 task("set-max-expiration", "Set max expiration time of RegistrarController")
   .addParam("time", "The max expiration time, 0 for unlimited", undefined, types.string)
-  .setAction(async ({ len }, hre) => {
+  .setAction(async ({ time }, hre) => {
     const [operator] = await hre.ethers.getSigners();
     const regController: RegistrarController = await hre.ethers.getContract("RegistrarController", operator);
-    const oldMinLen = await regController.minLengthAvailable();
-    console.log(`setMinLengthAvailable: ${oldMinLen} -> ${len}`);
-    const tx = await regController.setMinLengthAvailable(len);
+    const oldExp = await regController.maxExpirationTime();
+    const descOldExp = oldExp.isZero() ? "Unlimited" : new Date(oldExp.toNumber() * 1000);
+    const descNewExp = time == 0 ? "Unlimited" : new Date(time * 1000);
+    console.log(`setMaxExpirationTime: ${descOldExp} -> ${descNewExp}`);
+    const overrides = txParams(await operator.provider!.getFeeData(), await operator.getTransactionCount());
+    const tx = await regController.setMaxExpirationTime(time, overrides);
     console.log(`> tx: ${tx.hash}`);
     await tx.wait();
   });
