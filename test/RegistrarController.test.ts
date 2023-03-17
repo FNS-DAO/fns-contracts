342d7ae140b5454c18ea5b0d900d015615ca53b4
he <he@fns>
2023-03-17 09:46:56 +0800

    test RegistrarController

--- test/RegistrarController.test.ts
+++ test/RegistrarController.test.ts
@@ -77,7 +77,7 @@ describe("RegistrarController", () => {
     const expTime = nowTime + 365 * DAY;
 
     before(async function () {
-      registrarController.connect(owner).setRemainRegisterable(U256_MAX);
+      await registrarController.connect(owner).setRemainRegisterable(U256_MAX);
       // setMaxExpirationTime
       await registrarController.connect(owner).setMaxExpirationTime(expTime);
     });
@@ -118,6 +118,133 @@ describe("RegistrarController", () => {
       expect(await registrar.nameExpires(tid)).to.gte(nowTime + duration);
       expect(await registrar.balanceOf(user1.address)).to.eq(prevCount.add(1));
       expect(await registrar.ownerOf(tid)).to.eq(user1.address);
+    });
+  });
+
+  describe("setMinLengthAvailable", () => {
+    it("Non-owner can not setMinLengthAvailable()", async () => {
+      const originMinLengthAvailable = await registrarController.minLengthAvailable();
+      await expect(
+        registrarController.connect(user1).setMaxExpirationTime(originMinLengthAvailable.add(1)),
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+      // state should not be changed
+      expect(await registrarController.minLengthAvailable()).to.eq(originMinLengthAvailable);
+    });
+
+    it("Owner can setMinLengthAvailable()", async () => {
+      const originMinLengthAvailable = await registrarController.minLengthAvailable();
+      await registrarController.connect(owner).setMinLengthAvailable(originMinLengthAvailable.add(1));
+      expect(await registrarController.minLengthAvailable()).to.eq(originMinLengthAvailable.add(1));
+
+      for (let i = 1; i < 7; i++) {
+        await registrarController.connect(owner).setMinLengthAvailable(i);
+        // state should be changed
+        expect(await registrarController.minLengthAvailable()).to.eq(i);
+      }
+
+      await registrarController.connect(owner).setMinLengthAvailable(originMinLengthAvailable);
+      // state should be changed
+      expect(await registrarController.minLengthAvailable()).to.eq(originMinLengthAvailable);
+    });
+
+    it("Can not setMinLengthAvailable with 0", async () => {
+      await expect(registrarController.connect(owner).setMinLengthAvailable(0)).to.be.revertedWithCustomError(
+        registrarController,
+        "InvalidNameLength",
+      );
+    });
+  });
+
+  describe("Register with different length", () => {
+    const duration = 365 * DAY;
+    const minLen = 4;
+
+    before(async function () {
+      await registrarController.connect(owner).setRemainRegisterable(U256_MAX);
+      await registrarController.connect(owner).setMaxExpirationTime(0);
+      await registrarController.connect(owner).setMinLengthAvailable(minLen);
+    });
+
+    it("Can not register with length < minLengthAvailable", async () => {
+      const name = "sho";
+      await expect(register(user1, name, duration)).to.be.revertedWithCustomError(
+        registrarController,
+        "NameNotAvailable",
+      );
+    });
+
+    it("Can register with length = minLengthAvailable", async () => {
+      const name = "same";
+      const tid = labelhash(name);
+      const prevCount = await registrar.balanceOf(user1.address);
+      await register(user1, name, duration);
+      // check register result
+      expect(await registrar.balanceOf(user1.address)).to.eq(prevCount.add(1));
+      expect(await registrar.ownerOf(tid)).to.eq(user1.address);
+    });
+
+    it("Can register with length > minLengthAvailable", async () => {
+      const names = ["long", "longer", "register-with-long-name"];
+      for (let name of names) {
+        const tid = labelhash(name);
+        const prevCount = await registrar.balanceOf(user1.address);
+        await register(user1, name, duration);
+        // check register result
+        expect(await registrar.balanceOf(user1.address)).to.eq(prevCount.add(1));
+        expect(await registrar.ownerOf(tid)).to.eq(user1.address);
+      }
+    });
+  });
+
+  describe("setRemainRegisterable", () => {
+    it("Non-owner can not setRemainRegisterable()", async () => {
+      const originRemainRegisterable = await registrarController.remainRegisterable();
+      const count = 9023187542;
+      expect(originRemainRegisterable).to.not.eq(count);
+      await expect(registrarController.connect(user1).setRemainRegisterable(count)).to.be.revertedWith(
+        "Ownable: caller is not the owner",
+      );
+      // state should not be changed
+      expect(await registrarController.remainRegisterable()).to.eq(originRemainRegisterable);
+    });
+
+    it("Owner can setRemainRegisterable()", async () => {
+      const count = 123456789;
+      await registrarController.connect(owner).setRemainRegisterable(count);
+      // state should be changed
+      expect(await registrarController.remainRegisterable()).to.eq(count);
+
+      await registrarController.connect(owner).setRemainRegisterable(U256_MAX);
+      // state should be changed
+      expect(await registrarController.remainRegisterable()).to.eq(U256_MAX);
+
+      await registrarController.connect(owner).setRemainRegisterable(3);
+      // state should be changed
+      expect(await registrarController.remainRegisterable()).to.eq(3);
+    });
+  });
+
+  describe("withdraw", () => {
+    before(async function () {
+      registrarController.connect(owner).setRemainRegisterable(U256_MAX);
+      const duration = 365 * DAY;
+      await register(user1, "for-withdraw-only", duration);
+      expect(await provider.getBalance(registrarController.address)).to.be.gt(0);
+    });
+
+    it("Non-owner can not withdraw", async () => {
+      await expect(registrarController.connect(user1).withdraw(user1.address)).to.be.revertedWith(
+        "Ownable: caller is not the owner",
+      );
+    });
+
+    it("Owner can withdraw", async () => {
+      const prevContractBalance = await provider.getBalance(registrarController.address);
+      expect(prevContractBalance).to.be.gt(0);
+      const prevUserBalance = await provider.getBalance(user2.address);
+      await registrarController.connect(owner).withdraw(user2.address);
+      expect(await provider.getBalance(registrarController.address)).to.eq(0);
+      expect(await provider.getBalance(user2.address)).to.eq(prevUserBalance.add(prevContractBalance));
     });
   });
 
